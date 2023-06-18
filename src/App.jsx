a595e378e095ba5083a88511aaa01205273fb6e8
Vandeson Santos <vandeson.eng.quimica@gmail.com>
2023-06-18 01:29:51 -0300

    Components home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import "./components/social-networks/home.css"
 import Header from "./components/header/header"
-import SocialNetworks from "./components/social-networks/social-networks"
+import Home from "./components/home/home"
+
 
 function App() {
   return (
@@ -8,23 +8,8 @@ function App() {
       <section className="home">
         <Header />
 
-        <div className="conteiner">
-            <div className="display-flex full-screen .alinhar-itens-no-centro">
-                <div className="text-home">
-                    <h1>Vandeson Santos</h1>
-                    <h2>Desenvolvedor Frontend</h2>
-
-                    <SocialNetworks />
-                </div>
-
-                <div className="img-home">
-                    <div className="img-box inner-shadow">
-                        <img className="outer-shadow" src="src/image/foto-perfil.png" alt="foto do perfil" />
-                    </div>
-                </div>
-            </div>
-          </div>
-        </section>
+        <Home />
+      </section>
     </>
   )
 }
---
+++ src/components/home/home.jsx
@@ -0,0 +1,25 @@
+import "./home.css"
+import SocialNetworks from "../social-networks/social-networks"
+
+const Home = () => {
+    return (
+        <div className="conteiner">
+            <div className="display-flex full-screen .alinhar-itens-no-centro">
+                <div className="text-home">
+                    <h1>Vandeson Santos</h1>
+                    <h2>Desenvolvedor Frontend</h2>
+
+                    <SocialNetworks />
+                </div>
+
+                <div className="img-home">
+                    <div className="img-box inner-shadow">
+                        <img className="outer-shadow" src="src/image/foto-perfil.png" alt="foto do perfil" />
+                    </div>
+                </div>
+            </div>
+          </div>
+    )
+}
+
+export default Home
